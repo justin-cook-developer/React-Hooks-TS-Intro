d7a88f58ff7663ca6686dc5320caf4beb8e2f555
Justin Cook <justin.cook.dev@gmail.com>
2019-11-23 11:10:46 -0800

    Can add a todo with hooks!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const TodoForm = ({ addTodo }) => {
+  const [inputVal, setInputVal] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!inputVal) return;
+
+    addTodo(inputVal);
+    setInputVal('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <label htmlFor="todo">
+        <input
+          type="text"
+          id="todo"
+          className="input"
+          placeholder="Enter a todo..."
+          value={inputVal}
+          onChange={e => setInputVal(e.target.value)}
+        />
+      </label>
+
+      <label htmlFor="submit">
+        <button id="submit" type="submit">
+          Add Todo
+        </button>
+      </label>
+    </form>
+  );
+};
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn about React' },
+    { text: 'Meet with friend for lunch' },
+    { text: 'Build app for ryan!' },
+  ]);
+
+  const addTodo = text => setTodos([...todos, { text }]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((t, i) => (
+          <Todo key={i} index={i} todo={t} />
+        ))}
+
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
